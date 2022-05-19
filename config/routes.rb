0f48d7591395bb40ecbbd414005c63c875737e27
mockysunshinework <mockysunshinework@gmail.com>
2022-05-19 21:46:07 +0900

    static_pages_controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   get 'static_pages/top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'application#hello'
 end
